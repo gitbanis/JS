7be39226805ad4caa932fb5eea14524b57ec78e5
gitbanis <vvbanis@gmail.com>
2022-02-02 19:07:33 +0200

    Initial commit

---
+++ JS lesson_1/homework_1.js
@@ -0,0 +1,221 @@
+// // 1. Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let hel = 'hello'
+// let ow = 'owu'
+// let cm = 'com'
+// let uk = 'ua'
+// let one = 1
+// let ten = 10
+// let tos = -999
+// let hun = 123
+// let pi = 3.14
+// let two = 2.7
+// let sixten = 16
+// let bulean1 = 5 * 5 === 25
+// let bulean2 = 18/2 !== 9
+//
+// // 2. Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(hel)
+// console.log(ow)
+// console.log(cm)
+// console.log(uk)
+// console.log(one)
+// console.log(ten)
+// console.log(tos)
+// console.log(hun)
+// console.log(pi)
+// console.log(two)
+// console.log(sixten)
+// console.log(bulean1)
+// console.log(bulean2)
+//
+// // alert(hel)
+// // alert(ow)
+// // alert(cm)
+// // alert(uk)
+// // alert(one)
+// // alert(ten)
+// // alert(tos)
+// // alert(hun)
+// // alert(pi)
+// // alert(two)
+// // alert(sixten)
+// // alert(bulean1)
+// // alert(bulean2)
+//
+// document.write('<span class="let__one">' + hel + '</span>')
+// document.write('<span class="let__one">' + ow + '</span>')
+// document.write('<span class="let__one">' + cm + '</span>')
+// document.write('<span class="let__one">' + uk + '</span>')
+// document.write('<span class="let__one">' + one + '</span>')
+// document.write('<span class="let__one">' + ten + '</span>')
+// document.write('<span class="let__one">' + tos + '</span>')
+// document.write('<span class="let__one">' + hun + '</span>')
+// document.write('<span class="let__one">' + pi + '</span>')
+// document.write('<span class="let__one">' + two + '</span>')
+// document.write('<span class="let__one">' + sixten + '</span>')
+// document.write('<span class="let__one">' + bulean1 + '</span>')
+// document.write('<span class="let__one">' + bulean2 + '</span>')
+//
+// // 3. Переприсвоїти кожній змінній з завдання значення на довільне.
+// hel = 'Privet'
+// ow = 'Sory Babushka'
+// cm = 'club'
+// uk = 'kyiv'
+// one = one + ten
+// ten = 8
+// tos = tos + 500 * 3
+// hun = 'Cepasa senior'
+// pi = 'Number Pi'
+// two = one + tos
+// sixten = seventen = 70
+// bulean1 = ten/2 === 3
+// bulean2 = seventen-50 === 20
+//
+// // 4. Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(hel)
+// console.log(ow)
+// console.log(cm)
+// console.log(uk)
+// console.log(one)
+// console.log(ten)
+// console.log(tos)
+// console.log(hun)
+// console.log(pi)
+// console.log(two)
+// console.log(seventen)
+// console.log(bulean1)
+// console.log(bulean2)
+//
+// // alert(hel)
+// // alert(ow)
+// // alert(cm)
+// // alert(uk)
+// // alert(one)
+// // alert(ten)
+// // alert(tos)
+// // alert(hun)
+// // alert(pi)
+// // alert(two)
+// // alert(seventen)
+// // alert(bulean1)
+// // alert(bulean2)
+//
+// document.write('<span class="let__two">' + hel + '</span>')
+// document.write('<span class="let__two">' + ow + '</span>')
+// document.write('<span class="let__two">' + cm + '</span>')
+// document.write('<span class="let__two">' + uk + '</span>')
+// document.write('<span class="let__two">' + one + '</span>')
+// document.write('<span class="let__two">' + ten + '</span>')
+// document.write('<span class="let__two">' + tos + '</span>')
+// document.write('<span class="let__two">' + hun + '</span>')
+// document.write('<span class="let__two">' + pi + '</span>')
+// document.write('<span class="let__two">' + two + '</span>')
+// document.write('<span class="let__two">' + seventen + '</span>')
+// document.write('<span class="let__two">' + bulean1 + '</span>')
+// document.write('<span class="let__two">' + bulean2 + '</span>')
+//
+// //5. - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let firstName = 'Vladyslav'
+// let middleName = 'Vasylyovich'
+// let lastName = 'Banias'
+// let person = firstName + ' ' + middleName + ' ' + lastName
+//
+// // 6. За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// const nam = prompt('Як тебе звати?')
+// console.log(nam)
+// const fam = prompt('Яка твоя фамілія')
+// console.log(fam)
+// const age = prompt('Скільки тобі років')
+// console.log(age)
+// alert('Вітаю ' + nam + ' ' + fam + ', тобі ' + age + ' років')
+
+// // 7. За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100
+// let b = '100'
+// let c = true
+//
+// console.log(typeof a) // number type
+// console.log(typeof b) // tring type
+// console.log(typeof c) // boolean type
+//
+// // 8. Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// console.log(5<6)
+// console.log(5==6)
+// console.log(5===6)
+// console.log(5>6)
+// console.log(10===10)
+// console.log(10===10)
+// console.log(10>10)
+// console.log(10 < 10)
+// console.log(10!==10)
+// console.log(123>'123')
+// console.log(123==='123')
+//
+// // 24. Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20"; //тип текст
+// a = 5; // тип число
+// document.write('<span class="let__one">' + str + a +' '); // знак + працює математично тільки якщо всі типи математичні, в інших випадках він сполучає елементи(складає поряд)
+// document.write(str - a + ' '); // JS в математичних виразах, число яке записане як текст, сприймає як число (окрім знаку "+")
+// document.write(str * "2" + ' '); // JS в математичних виразах, число яке записане як текст, сприймає як число (окрім знаку "+")
+// document.write(str / 2 + '</span>'); //JS в математичних виразах, число яке записане як текст, сприймає як число (окрім знаку "+")
+
+// ClassWork Lesson 1
+//1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let ar = [10, 51, 23, 52, 14, 52.25, 85, 6, 312, -147]
+// let result = ar[0]+ar[1]+ar[2]+ar[3]+ar[4]+ar[5]+ar[6]+ar[7]+ar[8]+ar[9]
+// console.log(result)
+//
+// //2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//     name:'',
+//     pages:'',
+//     genre:''
+// }
+//
+// //3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// book = {
+//     name:'',
+//     pages:'',
+//     genre:'',
+//     author:'',
+// }
+//
+// //4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let arrBook = [
+//     {name: 'Фентезі книга', pages:'355', genre:'Фентезі', author:'Фентезі Письменник'},
+//     {name: 'Детектив детективний', pages:'148', genre:'Детектив', author:'Автор Детективний'},
+//     {name: 'Роман романтичний', pages:'144', genre:'Роман', author:'Автор Романіст'}
+// ]
+// console.log(arrBook)
+// let product1 = arrBook[0]
+// let product2 = arrBook[1]
+// let product3 = arrBook[1]
+//
+// console.log(product1)
+// console.log(product2)
+// console.log(product3)
+//
+// product1 = arrBook[0].name +', '+ arrBook[0].pages +', '+ arrBook[0].genre +', '+ arrBook[0].author
+// console.log(product1)
+
+//5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// let height = 23
+// let width = 10
+// let s = ((height * width)/1000) + ' м2'
+// console.log(s)
+//
+// //6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC), результат помістіть у змінну v.
+// let  heightC = 10
+// let dC = 4
+// let PI = 3.1415926535
+// let r2 = Math.pow(dC/2,2)
+// let v = PI * r2 * heightC //формула розрахунку обєму циландра
+// console.log(v)
+
+//7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+//        Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+// let n = 3
+// let m = 4
+// let k = Math.sqrt(Math.pow(n,2) + Math.pow(m,2))
+// console.log(k)
+
